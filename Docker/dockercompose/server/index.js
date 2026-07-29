8f57602fb1697dbd35e8b88939d4bb83e3a6279f
Upendra <tripathiupendra4321@gmail.com>
2026-07-29 21:00:42 +0530

    middleware implementation

--- Docker/dockercompose/server/index.js
+++ Docker/dockercompose/server/index.js
@@ -1,5 +1,12 @@
 import express from 'express'
 const app=express()
+app.use((req,res,next)=>{
+    console.log("this is a middleware");
+    next();
+})
+app.get('/',(req,res)=>{
+    res.send("this is my first work");
+})
 app.listen(5000,()=>{
     console.log("Actual writing of server port listening")
 })
