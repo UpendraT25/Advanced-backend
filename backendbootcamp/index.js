96acb20095f8bc1f2ee25d94039f2a65c17ed68b
Upendra <tripathiupendra4321@gmail.com>
2026-07-31 23:31:28 +0530

    showing benifit of asynchronous operation over syncronous

--- backendbootcamp/index.js
+++ backendbootcamp/index.js
@@ -31,17 +31,40 @@
 // })
 
 
+// const express=require('express')
+// const app=express();
+
+// app.get('/',(req,res)=>{
+//     res.status(200).json({data:"home page"})
+// })
+
+// app.get('/about',(req,res)=>{
+//     res.status(200).json({message:"about page"})
+// })
+
+// app.listen(5000,()=>{
+//     console.log('running on port 5000')
+// })
+
+// for the purpose of differentiating reading file synchronously and asynchronously
+const fs=require('fs');
 const express=require('express')
 const app=express();
 
-app.get('/',(req,res)=>{
-    res.status(200).json({data:"home page"})
+app.get('/sync',(req,res)=>{
+    fs.readFileSync('a.txt','utf-8',(err,data)=>{
+        console.log(data);
+    });
+    res.status(200).json({message:"synchronous endpoint"})
+})
+app.get('/async',(req,res)=>{
+    fs.readFile('a.txt',(err,data)=>{
+        console.log(data);
+    });
+    res.status(200).json({message:"asynchronous endpoint"})
 })
 
-app.get('/about',(req,res)=>{
-    res.status(200).json({message:"about page"})
-})
 
 app.listen(5000,()=>{
-    console.log('running on port 5000')
+    console.log('running on 5000 port')
 })
