4638bbf0f0c5ca5e72df73a2d1b9c7cfa4e27cfb
Upendra <tripathiupendra4321@gmail.com>
2026-09-11 23:20:02 +0530

    writing home page

--- Redis/index.js
+++ Redis/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 const app=express();
-
+app.post('/',(req,res)=>{
+    console.log("this is a home page")
+})
 app.listen(4000,()=>{
     console.log("this is call back")
 })
