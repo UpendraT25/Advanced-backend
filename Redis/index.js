c8dec806df7174a556a616a624456518f37be70a
Upendra <tripathiupendra4321@gmail.com>
2026-09-13 22:45:11 +0530

    redis catching strategy

--- Redis/index.js
+++ Redis/index.js
@@ -2,7 +2,8 @@ import express from 'express'
 const app=express();
 app.post('/',(req,res)=>{
     console.log("this is a home page")
+    console.log("reddis api catching")
 })
 app.listen(4000,()=>{
-    console.log("this is call back")
+    console.log("this is redis catching strategies")
 })
